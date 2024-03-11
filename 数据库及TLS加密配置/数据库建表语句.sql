f9b7f6e982e756e728c20897fb18d72b5ed19b86
Ethan <2553035342@qq.com>
2024-03-11 17:21:14 +0800

    数据库建表语句

---
+++ 数据库及TLS加密配置/数据库建表语句.sql
@@ -0,0 +1,73 @@
+CREATE DATABASE  IF NOT EXISTS `cloud_desktop` ;
+USE `cloud_desktop`;
+
+create table user
+(
+    id          bigint auto_increment comment '主键',
+    username    varchar(32) collate utf8_bin not null comment '用户名',
+    email       varchar(64) collate utf8_bin not null comment '邮箱',
+    phone       varchar(11) collate utf8_bin not null comment '手机号',
+    password    varchar(64) collate utf8_bin not null comment '密码',
+    create_time datetime default null        null comment '创建时间',
+    delete_time datetime default null        null comment '删除时间',
+    is_active boolean default true not null comment '账号是否启用',
+    PRIMARY KEY (`id`),
+    UNIQUE KEY (`username`)
+)
+    comment '用户表' collate = utf8_bin;
+
+create table roles
+(
+    id          bigint auto_increment comment '主键',
+    name        varchar(32) collate utf8_bin not null comment '角色名',
+    permissions tinyint                      not null comment '权限 1 管理员 2 用户权限',
+    primary key (`id`),
+    unique key (`name`)
+)
+    comment '权限表' collate = utf8_bin;
+
+create table user_roles
+(
+    id          bigint auto_increment comment '主键',
+    user_id     bigint                       not null comment '用户ID',
+    role_id     bigint                       not null comment '角色ID',
+    name        varchar(32) collate utf8_bin not null comment '角色名(冗余字段)',
+    permissions tinyint                      not null comment '权限(冗余字段)',
+    primary key (`id`)
+)
+    comment '用户角色关联表' collate = utf8_bin;
+
+create table image
+(
+    id          bigint auto_increment comment '主键',
+    image_id    varchar(60) collate utf8_bin  not null comment '镜像id',
+    name        varchar(50) collate utf8_bin  not null comment '镜像名',
+    image        varchar(255) collate utf8_bin default null null comment '镜像图片',
+    tag         varchar(10) collate utf8_bin  not null comment '镜像标签',
+    size        varchar(10) collate utf8_bin  not null comment '镜像大小',
+    create_time datetime default null         null comment '镜像创建时间',
+    delete_time datetime default null         null comment '镜像删除时间',
+    is_active   boolean  default true         not null comment '镜像是否启用',
+    owner_id    bigint                        not null comment '镜像所有者ID',
+    primary key (`id`),
+    unique key (`image_id`)
+)
+    comment '镜像表' collate = utf8_bin;
+
+create table container
+(
+    id           bigint auto_increment comment '主键',
+    container_id varchar(60) collate utf8_bin               not null comment '容器ID',
+    host_port    int                                        not null comment '主机端口映射',
+    image_id     bigint                                     not null comment '镜像ID',
+    name         varchar(32) collate utf8_bin               not null comment '容器名',
+    image        varchar(255) collate utf8_bin default null null comment '容器图片',
+    create_time  datetime                      default null null comment '容器创建时间',
+    delete_time  datetime                      default null null comment '删除时间',
+    owner_id     bigint                                     not null comment '所有人ID',
+    is_active    boolean                       default true not null comment '容器是否启用',
+    status       tinyint                       default 1    not null comment '容器状态',
+    primary key (`id`),
+    unique key (`container_id`)
+)
+    comment '容器表' collate = utf8_bin;
